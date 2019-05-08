3ae52ee05c703b6dceeb19d5b30debf12baf3965
anjalibandaru2 <47233220+anjalibandaru2@users.noreply.github.com>
2019-05-08 03:12:50 -0700

    Added a new folder latest code
    Added a new folder latest code

---
+++ LatestCode/flaskApi.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  9 17:10:26 2018
+
+@author: arkil
+"""
+
+import flask
+from flask import Flask, render_template, request
+from sklearn.externals import joblib
+from scipy import misc
+import numpy as np
+import pandas as pd
+from SFFirePredLibraries import preprocessInput
+from SFFirePredLibraries import getTrainColumns
+from SFFirePredLibraries import imputeColumnValues	
+app = Flask(__name__)
+@app.route("/")
+@app.route("/index")
+def index():
+   return flask.render_template('index.html')   
+@app.route('/predict', methods=['POST'])
+def make_prediction():
+    if request.method=='POST':
+        file = request.files['image']
+        if not file: return render_template('index.html', label="No file")
+        read_df = pd.read_csv(file, sep='\t')
+        file_inp = preprocessInput(read_df)
+        train_cols = getTrainColumns()
+        mod_test_df = imputeColumnValues(train_cols, file_inp)
+        print(file_inp.shape)
+        modelused = request.form["modelused"]
+        if modelused == "lasso":
+            prediction = lasso_model.predict(mod_test_df)
+        elif modelused == "randomforest":
+            prediction = rf_model.predict(mod_test_df)
+        else:
+            prediction = elastic_net_model.predict(mod_test_df)
+		#prediction = model.predict(mod_test_df)
+        label = str(np.squeeze(prediction))
+        return render_template('index.html', label=label)
+   
+if __name__ == '__main__':
+    lasso_model= joblib.load('rforest.pkl')
+    rf_model = joblib.load('rforest.pkl')
+    elastic_net_model = joblib.load('rforest.pkl')
+    app.run(host='0.0.0.0', port=8000, debug=True)
