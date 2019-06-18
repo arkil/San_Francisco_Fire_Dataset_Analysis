4aee0d1213b68dac5996a5b86e31e848fce2682c
Arkil Thakkar <thakkararkil1996@gmail.com>
2019-06-18 11:40:55 -0700

    Structuring folders

--- flaskApi.py
+++ flaskApi.py
@@ -11,43 +11,43 @@ from sklearn.externals import joblib
 from scipy import misc
 import numpy as np
 import pandas as pd
-from PreprocessingStep import preprocessInput
-from PreprocessingStep import getTrainColumns
-from PreprocessingStep import imputeColumnValues	
+from SFFirePredLibraries import preprocessInput
+from SFFirePredLibraries import getTrainColumns
+from SFFirePredLibraries import imputeColumnValues	
 app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-   return flask.render_template('index.html')   
+   return flask.render_template('index.html')
+
+   
 @app.route('/predict', methods=['POST'])
 def make_prediction():
-	if request.method=='POST':
-		file = request.files['image']
-		if not file: return render_template('index.html', label="No file")
-		
-		read_df = pd.read_csv(file, sep='\t')
-		#img = img[:,:,:3]
-		#img = img.reshape(1, -1)
-		file_inp = preprocessInput(read_df)
-		train_cols = getTrainColumns()
-        #TODO : modify gettraincolumns to remove suppression and return the result
-		mod_test_df = imputeColumnValues(train_cols, file_inp)
-		print(file_inp.shape)
-		#mod_test_df = mod_test_df.drop('Suppression Personnel')
-		# make prediction on new image
-		prediction = model.predict(mod_test_df)
-
-		# make prediction on new image
-
-	
-		# squeeze value from 1D array and convert to string for clean return
-		label = str(np.squeeze(prediction))
-
-		# switch for case where label=10 and number=0
-		# if label=='10': label='0'
-
-		return render_template('index.html', label=label)
+    if request.method=='POST':
+        file = request.files['image']
+        if not file: return render_template('index.html', label="No file")
+        read_df = pd.read_csv(file, sep='\t')
+        file_inp = preprocessInput(read_df)
+        train_cols = getTrainColumns()
+        mod_test_df = imputeColumnValues(train_cols, file_inp)
+        print(file_inp.shape)
+        modelused = request.form["modelused"]
+        if modelused == "lasso":
+            prediction = lasso_model.predict(mod_test_df)
+        elif modelused == "randomforest":
+            prediction = rf_model.predict(mod_test_df)
+        elif modelused == "ridge":
+            prediction = ridge_reg_model.predict(mod_test_df)
+        else:
+            prediction = gradient_boosting_model.predict(mod_test_df)
+		#prediction = model.predict(mod_test_df)
+        label = str(np.squeeze(prediction))
+        label = int(label)
+        return render_template('index.html', label=label)
    
 if __name__ == '__main__':
-    model = joblib.load('sf.pkl')
+    lasso_model= joblib.load('sf_reg_lasso.pkl')
+    rf_model = joblib.load('rforest.pkl')
+    ridge_reg_model = joblib.load('sf_reg_ridge.pkl')
+    gradient_boosting_model = joblib.load('sf_reg_gbr.pkl')
     app.run(host='0.0.0.0', port=8000, debug=True)
